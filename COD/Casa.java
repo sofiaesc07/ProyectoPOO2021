5cd6d3af280d9413af84883f27d6cab704e6e536
Asael211146 <91224136+Asael211146@users.noreply.github.com>
2021-09-30 11:44:43 -0600

    Add files via upload

--- COD/Casa.java
+++ COD/Casa.java
@@ -1,4 +1,4 @@
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class Casa {
     //Atributos
@@ -8,9 +8,15 @@ public class Casa {
 
     //Método para que el usuario digite
     public void leerNumeros() {
-        numero1 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la cantidad de kWh que genera en su hogar, esto lo puede encontrar en su recibo de luz: "));
+        
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de kWh que genera en su hogar, esto lo puede encontrar en su recibo de luz: ");
+        numero1 = entrada.nextDouble();
         numero2 = 0.385;
+        entrada.close();
     }
+        
 
     public void multiplicar() {
         multiplicacion = numero1 * numero2;
@@ -18,6 +24,6 @@ public class Casa {
     }
 
     public void Resultado(){
-        JOptionPane.showMessageDialog(null, "La cantidad de co2 que generas en tu hogar es de: "+multiplicacion+" kg de co2");
+        System.out.println("La cantidad de co2 que generas en tu hogar es de: "+multiplicacion+" kg de co2");
     }
 }
