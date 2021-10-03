76daca345ec5668c92174e59e053bfc767320b19
Asael211146 <91224136+Asael211146@users.noreply.github.com>
2021-10-03 01:30:09 -0600

    Add files via upload

---
+++ COD_agua/ResultadosCasa_agua.java
@@ -0,0 +1,11 @@
+public class ResultadosCasa_agua {
+    public static void main(String[] args) {
+
+        Casa_agua cs1 = new Casa_agua();
+
+        cs1.leerNumeros();
+        cs1.multiplicar();
+        cs1.Resultado();
+
+    }
+}
