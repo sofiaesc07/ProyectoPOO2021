f89e606d717a3ca5621dac44e7851ab7c3d251a7
TuPapiErick69 <90981970+TuPapiErick69@users.noreply.github.com>
2021-10-03 20:51:38 -0600

    Add files via upload

---
+++ Comidas.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Comidas {
+    //Atributos
+    double numero1;
+    double multiplicacion;
+    double numero2;
+
+  //Cuerpo del programa. En el caso de las comidas solo se puede dar un valor aproximado debido a que no existe un factor estandarizado.
+	//}
+    
+    public double getNumero1(){
+    return numero1;
+   }
+
+    public void leerNumeros() {
+        
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de comidas que hace al día (normalmente de tres a cinco): ");
+        numero1 = entrada.nextDouble();
+        numero2 = 1.69;
+        multiplicacion = numero1 * numero2; //Operación para calcular la huella de carbono de las comidas   
+    }
+        
+    public void Resultado(){
+        System.out.println("La huella de carbono aproximada que generas diariamente al comer es de: "+multiplicacion+ " kg de CO2.");
+    }
+}
